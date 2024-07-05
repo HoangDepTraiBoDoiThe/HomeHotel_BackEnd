4e56e145c8df9b85e312f9a7f0a84ac33a0d2f3b
HoangDepTraiBoDoiThe <123256016+HoangDepTraiBoDoiThe@users.noreply.github.com>
2024-07-05 17:34:42 +0700

    Initialize

---
+++ src/test/java/dev/hoang/homehotel/HomeHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hoang.homehotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
