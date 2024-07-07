4915fd6f27fceaf0e53a7cd15df6ee38d2f62605
HoangDepTraiBoDoiThe <Mydrawingartist@gmail.com>
2024-07-07 11:49:15 +0700

    Modified Cors setting

--- src/main/java/dev/hoang/homehotel/CorsConfig.java
+++ src/main/java/dev/hoang/homehotel/CorsConfig.java
@@ -9,9 +9,9 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // Your React app's origin
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:5173") // Replace with your React app's URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
